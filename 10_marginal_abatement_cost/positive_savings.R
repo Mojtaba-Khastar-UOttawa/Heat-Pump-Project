cbb29c4359e7bec9c690a29b1b96726a7321f064
MOJTABA KHASTAR <mkhas042@uottawa.ca>
2025-03-06 13:45:30 -0500

    Delete 10_marginal_abatement_cost directory

--- 10_marginal_abatement_cost/positive_savings.R
+++
@@ -1,9 +0,0 @@
-positive_savings <- homes_heating %>%
-  filter(saving > 0)
-percentage_houses_positive_savings <- nrow(positive_savings)/nrow(homes_heating) * 100
-cat("Percentage of houses with positive cost savings:", percentage_houses_positive_savings, "%\n")
-
-positive_ghg <- homes_heating %>%
-  filter(ghg > 0)
-percentage_houses_positive_ghg <- nrow(positive_ghg)/nrow(homes_heating) * 100
-cat("Percentage of houses with positive GHG savings:", percentage_houses_positive_ghg, "%\n")
